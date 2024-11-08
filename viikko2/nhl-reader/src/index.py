0fd560182b07e418e6b2addf771f453221b000e6
Jesse Rauha <jesse.rauha@gmail.com>
2024-11-08 23:46:34 +0200

    W2 E12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,14 @@
-import requests
-from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    print("Players from FIN:\n")
-    fin_players = filter(lambda p: p.nationality == "FIN", players)
-    # Order by points
-    fin_players = sorted(fin_players, key=lambda p: p.points, reverse=True)
-    for player in fin_players:
+    for player in players:
         print(player)
 
 
---
+++ viikko2/nhl-reader/src/player_stats.py
@@ -0,0 +1,8 @@
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players = self.reader.get_players()
+        players = filter(lambda p: p.nationality == nationality, players)
+        return sorted(players, key=lambda p: p.points, reverse=True)
